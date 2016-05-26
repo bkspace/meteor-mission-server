cc76353d309021cfd58894bc48e19dcd4cfaf526
Gordan <me@ggordan.com>
2016-05-26 15:49:36 +0100

    Quad update

--- server/index.js
+++ server/index.js
@@ -3,23 +3,37 @@ import cors from 'cors';
 import Quadrant from './quadrant';
 import bodyParser from 'body-parser';
 import { shoppingList } from './database';
+
 const q = new Quadrant();
-
 const app = express();
-
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-app.post('/locations', urlencodedParser, (req, res) => {
-});
+const jsonParser = bodyParser.json();
 
 const formatMessage = (msg) => `data: ${msg}\n\n`;
+
+function getIdFromBeacons(beacons = []) { // [1,2,3]
+  const quads = {
+    '123': 'one', // F is the quadrant
+    '234': 'two',
+    '345': 'three',
+    '467': 'four',
+    '247': 'five',
+    '278': 'six',
+  };
+
+  const sortedBeacons = beacons.sort().join('');
+
+  if (quads[sortedBeacons]) {
+    return quads[sortedBeacons];
+  }
+}
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Starting server on 3000');
 });
 
-app.get('/update', (req, res) => {
-  q.update(Math.random());
+app.post('/update', jsonParser, (req, res) => {
+  const newQuad = getIdFromBeacons(req.body);
+  q.update(newQuad);
   res.end('ok');
 });
 

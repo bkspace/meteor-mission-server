b49d0bc838828b0edcfc4c01d3fcb11888b7e43b
Gordan <me@ggordan.com>
2016-05-26 13:30:16 +0100

    Cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Starting server on 3000');
+});
+
+app.get('/connect', cors(), function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  setInterval(() => {
+    res.write("This is my data");
+  }, 3000);
 });

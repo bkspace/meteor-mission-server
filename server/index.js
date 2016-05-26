b13affe41b4e895bc1d5c5cff2b000cdbc3c06b1
Gordan <me@ggordan.com>
2016-05-26 13:44:24 +0100

    add formatMessage function

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 
 const app = express();
 
+const formatMessage = (msg) => `data: ${msg}\n\n`;
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Starting server on 3000');
 });
@@ -14,6 +16,6 @@ app.get('/connect', cors(), function(req, res) {
     'Connection': 'keep-alive'
   });
   setInterval(() => {
-    res.write("data: My message\n\n");
+    res.write(formatMessage("123"));
   }, 3000);
 });
